1098992ddf9ce126de464207c18054752d57c2dd
markus <markus.laanes@voco.ee>
2026-01-15 01:36:04 +0200

    Ülesanne 4, nüüd annab filtreerida aastate järgi

--- ReactProject/src/components/Expenses/Expenses.jsx
+++ ReactProject/src/components/Expenses/Expenses.jsx
@@ -1,11 +1,29 @@
 import "./Expenses.css";
 import ExpensesItem from "./ExpenseItem";
 import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
+import { useState } from "react";
 
 const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState("2023");
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+    console.log(selectedYear);
+  };
+
+  const filteredExpenses = props.expenses.filter(
+    (expense) => expense.date.getFullYear().toString() === filteredYear
+  );
+
   return (
     <Card className="expenses">
-      {props.expenses.map((expense, index) => (
+      <ExpensesFilter
+        selected={filteredYear}
+        onChangeFilter={filterChangeHandler}
+      />
+
+      {filteredExpenses.map((expense, index) => (
         <ExpensesItem key={index} data={expense} />
       ))}
     </Card>
